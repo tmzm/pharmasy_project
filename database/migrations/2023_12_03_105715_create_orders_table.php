fd984897aa6fbc9b35fce6ea7ce65382411e0416
tareq mozayek <tareqmozayek@gmail.com>
2024-01-03 11:48:37 +0300

    notifyWeb12

--- database/migrations/2023_12_03_105715_create_orders_table.php
+++ database/migrations/2023_12_03_105715_create_orders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->enum('status',['preparing','shipping','delivered'])->default('sent');
+            $table->enum('status',['preparing','shipping','delivered'])->default('preparing');
             $table->boolean('payment_status')->default(false);
             $table->decimal('total_price')->nullable();
             $table->timestamps();
